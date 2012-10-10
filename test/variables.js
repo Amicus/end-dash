3aebfa5c344e75ee5ae0549b73234b992e6ecadd
Zach Smith <zach@amicushq.com>
2012-10-09 23:25:31 -0400

    traverse the inside of elements

--- test/variables.js
+++ test/variables.js
@@ -4,15 +4,22 @@ var path = require("path")
 scriptModule(path.join(__dirname, "..", "lib", "end-dash.js"))
 
 describe("Setting a single variable", function() {
-  var template
-
-  beforeEach(function () {
-    var EndDash = window.require("/lib/end-dash") 
-    template = new EndDash('<div class = "singleVariable-"></div>')
-  })
 
   it("should be set in the html", function () {
+    var EndDash = window.require("/lib/end-dash") 
+      , template = new EndDash('<div class = "singleVariable-"></div>')
     template.set("singleVariable", "this is value")
+
+    $("body").append(template.template)
+    expect($(".singleVariable-").html()).to.be("this is value")
+  })
+
+  it("should be set in the html even when nested in other elements", function () {
+    var EndDash = window.require("/lib/end-dash") 
+      , template = new EndDash('<div><div class = "singleVariable-"></div></div>')
+
+    template.set("singleVariable", "this is value")
+
     $("body").append(template.template)
     expect($(".singleVariable-").html()).to.be("this is value")
   })
