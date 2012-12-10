f08a31aebf6789ed197c157c41eed5198f222c0f
Zach Smith <zach@amicushq.com>
2012-12-10 16:21:42 -0500

    update enumerable test for Factory syntax

--- test/enumerables.js
+++ test/enumerables.js
@@ -5,16 +5,20 @@ var path = require("path")
 script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
 script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
 script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "util.js"), { module: true })
 
 describe("An enumerable template", function() {
 
   it("should set all the values in the html", function () {
-    var EndDash = window.require("/lib/end-dash") 
-      , template = new EndDash(fs.readFileSync(__dirname + "/support/enumerable.html").toString())
+    var TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator(fs.readFileSync(__dirname + "/support/enumerable.html").toString()).generate()
+
+    template = new Template
 
     template.set("people", [{name: "Zach"}, {name: "Dog"}])
 
     $("body").append(template.template)
+
     expect($(".people- .person-:nth-child(1) .name-").html()).to.be("Zach")
     expect($(".people- .person-:nth-child(2) .name-").html()).to.be("Dog")
   })
