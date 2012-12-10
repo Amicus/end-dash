dcbb1bcc7ddf37ae1f092ecae7df7aea4a95f309
Zach Smith <zach@amicushq.com>
2012-12-10 12:03:00 -0500

    more get selector tests

--- test/get-selector.js
+++ test/get-selector.js
@@ -1,8 +1,19 @@
 var Template = require("../lib/end-dash")
+  , path = require("path")
+  , expect = require("expect.js")
+  , fs = require("fs")
+
+script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
 
 describe("getSelector", function() {
   it("should return a selector unique within root", function() {
-    //iterate over a large document, and check that each selector matches only
-    //the one element that we want
+    var html = '<div class = "hi"><span></span><span><div class = "woot"</span></div>'
+      , $ = window.$
+    window.document.body.innerHTML = html
+    var root = $(".hi")
+    var element = $(".woot")
+    console.log(Template.getSelector(element, root))
   })
 })
