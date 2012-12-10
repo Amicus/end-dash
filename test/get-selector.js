2f8e10a59656386f8ea03e37377fbd49b8eb2a9a
Zach Smith <zach@amicushq.com>
2012-12-10 12:16:15 -0500

    test use util and a test using id to shorten selector

--- test/get-selector.js
+++ test/get-selector.js
@@ -1,11 +1,8 @@
-var Template = require("../lib/end-dash")
+var util = require("../lib/util")
   , path = require("path")
   , expect = require("expect.js")
-  , fs = require("fs")
 
-script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
-script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
-script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "util.js"), { module: true })
 
 describe("getSelector", function() {
   it("should return a selector unique within root", function() {
@@ -13,7 +10,19 @@ describe("getSelector", function() {
       , $ = window.$
     window.document.body.innerHTML = html
     var root = $(".hi")
-    var element = $(".woot")
-    console.log(Template.getSelector(element, root))
+      , element = $(".woot")
+
+    expect($(util.getSelector(element, root), root).is(element)).to.be(true)
+  })
+  it("should use id as a base when it finds an id", function() {
+    var html = '<div class = "hi"><span></span><span><div id = "hey"><span class = "woot"></span></div></div>'
+      , $ = window.$
+    window.document.body.innerHTML = html
+
+    var root = $(".hi")
+      , element = $(".woot")
+
+    expect($(util.getSelector(element, root), root).is(element)).to.be(true)
+    expect(util.getSelector(element, root)).to.be("#hey > .woot")
   })
 })
