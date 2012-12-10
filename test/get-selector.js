0c4df6a1861c93f9ed4de9c5d10481bd8b7807fe
Zach Smith <zach@amicushq.com>
2012-12-09 20:42:21 -0500

    method to get a unique css selector for a given element, relative to some root element

---
+++ test/get-selector.js
@@ -0,0 +1,8 @@
+var Template = require("../lib/end-dash")
+
+describe("getSelector", function() {
+  it("should return a selector unique within root", function() {
+    //iterate over a large document, and check that each selector matches only
+    //the one element that we want
+  })
+})
