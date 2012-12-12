c5780af217ad80162372acb949bc4481a5330529
Zach Smith <zach@amicushq.com>
2012-12-12 11:39:30 -0500

    binders all the way down

--- test/bind.js
+++ test/bind.js
@@ -1,7 +1,9 @@
 var path = require("path")
   , expect = require("expect.js")
   , fs = require("fs")
-  , Model = require("backbone").Model
+  , Backbone = require("backbone")
+  , Model = Backbone.Model
+  , Collection = Backbone.Collection
   , ed = require("../lib/end-dash")
 
 script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
@@ -19,5 +21,16 @@ describe("When I initialize a template with a model", function() {
 
     $(window.document.body).append(template.template)
     expect($(".ohHi-").html()).to.be("Hello There")
+  })
+  it("should bind to a collection", function () {
+    var models = new Collection([new Model({ name: "Hawg" }), new Model({ name: "Dawg" })])
+      , TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator('<ul class="peeps-"><li class = "peep-"><div class="name-"></div></li></ul>').generate()
+      , template = new Template({ peeps: models })
+      , $ = window.$
+
+    $(window.document.body).append(template.template)
+    expect($(".peeps- li:nth-child(1) .name-").html()).to.be("Hawg")
+    expect($(".peeps- li:nth-child(2) .name-").html()).to.be("Dawg")
   })
 })
