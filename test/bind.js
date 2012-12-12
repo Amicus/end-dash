0e31a31a03e74237e9254f74e72fc5a15dfcb02a
Zach Smith <zach@amicushq.com>
2012-12-12 10:44:34 -0500

    mixin and matchin pojso's and backbone models aint no thang

---
+++ test/bind.js
@@ -0,0 +1,23 @@
+var path = require("path")
+  , expect = require("expect.js")
+  , fs = require("fs")
+  , Model = require("backbone").Model
+  , ed = require("../lib/end-dash")
+
+script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "util.js"), { module: true })
+ 
+describe("When I initialize a template with a model", function() {
+  it("should bind to the values", function () {
+    var model = new Model({ ohHi: "Hello There" })
+      , TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator('<div class = "ohHi-"></div>').generate()
+      , template = new Template("ohHi")
+      , $ = window.$
+
+    $(window.document.body).append(template.template)
+    expect($("ohHi-").html()).to.be("Hello There")
+  })
+})
