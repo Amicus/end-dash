9c7b0fcc6c9da969b98dee7ad234af10d4eec399
Zach Smith <zach@amicushq.com>
2012-12-12 14:51:45 -0500

    make sure it properly builds a view heirarchy

--- test/view-construction.js
+++ test/view-construction.js
@@ -11,8 +11,8 @@ script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
 script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
 script(path.join(__dirname, "..", "lib", "util.js"), { module: true })
  
-describe("When I initialize a template with a model", function() {
-  it("should bind to the values", function(done) {
+describe("When I initialize a template with a view bound to it", function() {
+  it("it should initialize the view correctly", function(done) {
     var model = new Model({ ohHi: "Hello There" })
       , TemplateGenerator = window.require("/lib/end-dash")
       , $ = window.$
@@ -35,6 +35,39 @@ describe("When I initialize a template with a model", function() {
     window.require.modules["/test/views/test_view.js"] = { exports: MockView }
 
     Template = new TemplateGenerator('<div class = "thing- testView-"></div>').generate()
+    template = new Template(model)
+  })
+
+   it("should setup the view heirarchy correctly", function(done) {
+    var model = new Model({ ohHi: "Hello There" })
+      , TemplateGenerator = window.require("/lib/end-dash")
+      , $ = window.$
+      , Template
+      , template
+
+    TemplateGenerator.configure({ viewDirectory: "/test/views" })
+
+    var parentInstance
+    function Parent() {
+      parentInstance = this
+    }
+
+    function MockView(opts) {
+      expect(this).to.be.a(MockView)
+      expect(opts.model).to.be(model)
+      expect(opts.parent).to.be.a(Parent)
+      expect(opts.parent).to.be(parentInstance)
+      //next tick because we have to allow the template constructor to return
+      //in order to check that it passed itself in
+      process.nextTick(function() {
+        expect(opts.template).to.be(template)
+        done()
+      })
+    }
+    window.require.modules["/test/views/test_view.js"] = { exports: Parent }
+    window.require.modules["/test/views/embedded_test_view.js"] = { exports: MockView }
+
+    Template = new TemplateGenerator('<div class = "testView-"><div class = "thing- embeddedTestView-"></div></div>').generate()
     template = new Template(model)
   })
 })
