52fa007d4d78176f0ebd3a8b5a78f9fd05498558
Zach Smith <zach@amicushq.com>
2012-12-13 08:43:25 -0500

    test for conditional tags

---
+++ test/conditional_tag.js
@@ -0,0 +1,28 @@
+var path = require("path")
+  , expect = require("expect.js")
+  , fs = require("fs")
+
+script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "util.js"), { module: true })
+
+describe("A conditional tag", function() {
+
+  it("should not be visible when false", function () {
+    var TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator('<div class="isSet-"></div>').generate()
+      , template = new Template
+
+    $("body").append(template.template)
+
+    expect($("isSet-").is(":visible")).to.be(true)
+
+    template.set("set", false)
+    expect($("isSet-").is(":visible")).to.be(false)
+
+    template.set("set", true)
+    expect($("isSet-").is(":visible")).to.be(true)
+  })
+
+})
