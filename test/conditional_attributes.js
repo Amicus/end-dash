60d7a1b312e74d141532a2e3a492e179206e158e
Zach Smith <zach@amicushq.com>
2012-12-13 13:07:21 -0500

    add the false side of the attribute ternary

--- test/conditional_attributes.js
+++ test/conditional_attributes.js
@@ -24,4 +24,18 @@ describe("A conditional attribute", function() {
     template.set("set", true)
     expect($("#el").attr("class")).to.be("omgYes")
   })
-}) 
+
+  it("should handle else replacement values", function() {
+    var TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator('<div id="el" class="#{set ? omgYes : omgNo }"></div>').generate()
+      , template = new Template
+    
+    $("body").append(template.template)
+        
+    template.set("set", false)
+    expect($("#el").attr("class")).to.be("omgNo")
+    
+    template.set("set", true)
+    expect($("#el").attr("class")).to.be("omgYes")
+  })
+  }) 
