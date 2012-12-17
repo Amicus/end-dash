e00ca73f741a60a20d8712b51e9770fe5d4fab15
Zach Smith <zach@amicushq.com>
2012-12-16 20:41:16 -0500

    test for embedded models in templates

---
+++ test/embedded_models.js
@@ -0,0 +1,28 @@
+var path = require("path")
+  , expect = require("expect.js")
+  , fs = require("fs")
+
+script(path.join(__dirname, "..", "lib", "end-dash.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "collection.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "parser.js"), { module: true })
+script(path.join(__dirname, "..", "lib", "util.js"), { module: true })
+
+describe("An enumerable template", function() {
+
+  it("should set all the values in the html", function () {
+    var TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator(fs.readFileSync(__dirname + "/support/embedded_models.html").toString()).generate()
+      , template = new Template
+
+    template.set({ name: "A Survey", person: { name: "Zach" }})
+
+    $("body").append(template.template)
+
+    expect($("#surveyName").html()).to.be("A Survey")
+
+    console.log(jqts(template.template))
+    expect($(".person- .name-").html()).to.be("Zach")
+
+  })
+
+})
