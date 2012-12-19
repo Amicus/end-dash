535f106cef077fb429ccbbf76f10c60a8c457cbc
Zach Smith <zach@amicushq.com>
2012-12-19 15:54:17 -0500

    test for reset functionality

--- test/backbone_integration.js
+++ test/backbone_integration.js
@@ -43,5 +43,38 @@ describe("when integrating with backbone", function() {
     expect($(".script- .question-:nth-child(3) > .answer- > .name-").html()).to.be("a3")
     done()
   })
+
+  it("it should update the collection after reset", function(done) {
+    var models = [
+      new Backbone.Model({ name: "q1", answer: new Backbone.Model({ name: "a1" }) }), 
+      new Backbone.Model({ name: "q2", answer: new Backbone.Model({ name: "a2" }) }), 
+      new Backbone.Model({ name: "q3", answer: new Backbone.Model({ name: "a3" }) })
+    ]
+    var questions = new Backbone.Collection()
+    var script = new Backbone.Model({ name: "the name", questions: questions })
+      , markup = fs.readFileSync(__dirname + "/support/complex_nested.html").toString()
+      , TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator(markup).generate()
+      , template = new Template({ script: script }) 
+
+    $("body").html(template.template)
+
+    questions.reset(models)
+    expect($(".script- .name-:nth-child(1)").html()).to.be("the name")
+
+    expect($(".script- .question-:nth-child(1) > .name-").html()).to.be("q1")
+    expect($(".script- .question-:nth-child(2) > .name-").html()).to.be("q2")
+    expect($(".script- .question-:nth-child(3) > .name-").html()).to.be("q3")
+
+    expect($(".script- .question-:nth-child(1) > .answer- > .name-").html()).to.be("a1")
+    expect($(".script- .question-:nth-child(2) > .answer- > .name-").html()).to.be("a2")
+    expect($(".script- .question-:nth-child(3) > .answer- > .name-").html()).to.be("a3")
+    done()
+  }) 
+
+/*
+ * <div class = "isThing- script-"> this will try to build the selector for isThing- 
+ * from within the contents of script-, which it will never find!
+ **/
 })
 
