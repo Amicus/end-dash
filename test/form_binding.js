5467a754ac01c27b9e96bbe9100936c3fd3774e0
Zach Smith <zach@amicushq.com>
2013-02-10 20:14:51 -0500

    form binding back in

--- test/form_binding.js
+++ test/form_binding.js
@@ -4,17 +4,17 @@ var path = require("path")
   , Backbone = require("backbone")
 
 describe("when the template has a form element", function() {
-it("should update the model when it changes to it's change")
-//  var model = new Backbone.Model({ name: "old" })
-//    , markup = '<div class = "model-"><input type="text" class = "name-"></div>'
-//    , TemplateGenerator = window.require("/lib/end-dash")
-//    , Template = new TemplateGenerator(markup).generate()
-//    , template = new Template({ model: model })
-//
-//  $("body").html(template.template)
-//
-//  $(".name-").val("new").change()
-//
-//  expect(model.get("name")).to.be("new")
-//})
-}) 
+  it("should update the model when it changes to it's change", function() {
+    var model = new Backbone.Model({ name: "old" })
+      , markup = '<div class = "model-"><input type="text" class = "name-"></div>'
+      , TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator(markup).generate()
+      , template = new Template({ model: model })
+
+    $("body").html(template.template)
+
+    $(".name-").val("new").change()
+
+    expect(model.get("name")).to.be("new")
+  })
+})
