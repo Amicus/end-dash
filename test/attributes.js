8b0448baa727bca4b582a294e49134493cca03b1
Zach Smith <zach@amicushq.com>
2013-02-11 16:12:35 -0500

    Implement attribute interpolation feature. with @adammck

--- test/attributes.js
+++ test/attributes.js
@@ -2,28 +2,23 @@ var path = require("path")
   , expect = require("expect.js")
   , fs = require("fs")
 
-//describe("An element with an attribute", function() {
-//  it("should set the attribute", function () {
-//    var TemplateGenerator = window.require("/lib/end-dash")
-//      , Template = new TemplateGenerator(fs.readFileSync(__dirname + "/support/attributes.html").toString()).generate()
-//      , template = new Template
-//
-//    template.set("name", "zach")
-//
-//    $("body").append(template.template)
-//    expect($("#link").attr("href")).to.be("/person/zach")
-//  })
-//})
-//describe("An element with multiple attributes", function() {
-//  it("should set the attribute")
-//  var TemplateGenerator = window.require("/lib/end-dash")
-//    , Template = new TemplateGenerator('<a id = "link" href = "#{one} and #{two}"></a>').generate()
-//    , template = new Template
-//
-//  template.set("one", "1")
-//  template.set("two", "2")
-//
-//  $("body").append(template.template)
-//  expect($("#link").attr("href")).to.be("1 and 2")
-//})
-//})
+describe("An element with an attribute", function() {
+  it("should set the attribute", function () {
+    var TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator(fs.readFileSync(__dirname + "/support/attributes.html").toString()).generate()
+      , template = new Template({ name: "zach" })
+
+    $("body").append(template.template)
+    expect($("#link").attr("href")).to.be("/person/zach")
+  })
+})
+describe("An element with multiple attributes", function() {
+  it("should set the attribute", function() {
+    var TemplateGenerator = window.require("/lib/end-dash")
+      , Template = new TemplateGenerator('<a id="link" href="#{one} and #{two}"></a>').generate()
+      , template = new Template({ one: "1", two: "2" })
+
+    $("body").append(template.template)
+    expect($("#link").attr("href")).to.be("1 and 2")
+  })
+})
