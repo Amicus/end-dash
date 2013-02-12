ed53447f11f870098fad4daaed9f1e3ae6d677c3
Zach Smith <zach@amicushq.com>
2013-02-12 02:53:43 -0500

    Refactorizor

--- test/partials.js
+++ test/partials.js
@@ -4,27 +4,28 @@ var path = require("path")
   , Backbone = require("backbone")
   , _ = require("underscore")
   , jqts = require("../lib/util").jqts
+  , EndDash = require("../lib/end-dash")
+  , generateTemplate = require("./util").generateTemplate
 
 describe("A template with partials", function() {
-  it("should collections", function() {
-    //use node's path in the browser env
-    var EndDash = window.require("/lib/end-dash")
-
+  it("should do collections", function() {
     var templates = [
       "/support/partials.html",
       "/support/embedded_partial.html",
       "/support/list_item.html"
     ]
 
+    var model = { 
+      items: new Backbone.Collection([{ variable: "wat1" }, { variable: "wat2" }]),
+      thing: new Backbone.Model({ name: "Zach" }) 
+    }
+
     _(templates).each(function(template) {
       EndDash.registerTemplate(template, fs.readFileSync(__dirname + template).toString())
     })
 
-    var Template = EndDash.getTemplate("/support/partials.html")
-      , items = new Backbone.Collection([{ variable: "wat1" }, { variable: "wat2" }])
-      , template = new Template({ items: items, thing: new Backbone.Model({ name: "Zach" }) })
+    var template = generateTemplate(model, "/support/partials.html")
 
-    $("body").html(template.template)
     expect($(".items- .item-:nth-child(1) .variable-").html()).to.be("wat1")
     expect($(".items- .item-:nth-child(2) .variable-").html()).to.be("wat2")
     expect($(".thing- .name-").html()).to.be("Zach")
--- test/util.js
+++ test/util.js
@@ -1,10 +1,14 @@
 function generateTemplate(model, markup) {
   var EndDash = require("../lib/end-dash")
-    , Template = new EndDash(markup).generate()
-    , template = new Template(model)
+    , Template, template
+  if(markup.charAt(0) === '/') {
+    Template = new EndDash.getTemplate(markup)
+  } else {
+    Template = new EndDash(markup).generate() 
+  }
+  template = new Template(model) 
 
   $("body").html(template.template)
-
   return template
 }
 module.exports = { generateTemplate: generateTemplate }
