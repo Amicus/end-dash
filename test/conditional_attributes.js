61d14dace45f859965a2d315914a9429ad0ef434
Zach Smith <zach@amicushq.com>
2013-02-11 22:42:54 -0500

    Conditional attributes work

--- test/conditional_attributes.js
+++ test/conditional_attributes.js
@@ -1,36 +1,40 @@
 var path = require("path")
   , expect = require("expect.js")
   , fs = require("fs")
+  , Backbone = require("backbone")
 
 describe("A conditional attribute", function() {
+  beforeEach(function() {
+    this.TemplateGenerator = window.require("/lib/end-dash")
+    this.model = new Backbone.Model({})
+  })
 
-//it("should be set correctly", function () {
-//  var TemplateGenerator = window.require("/lib/end-dash")
-//    , Template = new TemplateGenerator('<div id="el" class="#{set ? omgYes}"></div>').generate()
-//    , template = new Template
-//
-//  $("body").append(template.template)
-//
-//  expect($("#el").attr("class")).to.be("")
-//
-//  template.set("set", true)
-//  expect($("#el").attr("class")).to.be("omgYes")
-//
-//  template.set("set", false)
-//  expect($("#el").attr("class")).to.be("")
-//})
-//
-//it("should handle else replacement values", function() {
-//  var TemplateGenerator = window.require("/lib/end-dash")
-//    , Template = new TemplateGenerator('<div id="el" class="#{set ? omgYes : omgNo }"></div>').generate()
-//    , template = new Template
-//  
-//  $("body").append(template.template)
-//      
-//  template.set("set", false)
-//  expect($("#el").attr("class")).to.be("omgNo")
-//  
-//  template.set("set", true)
-//  expect($("#el").attr("class")).to.be("omgYes")
-//})
+  it("should be set correctly", function () {
+    var Template = new this.TemplateGenerator('<div id="el" class="#{set ? omgYes}"></div>').generate()
+      , template = new Template(this.model)
+
+    $("body").append(template.template)
+
+    expect($("#el").attr("class")).to.be("")
+
+    this.model.set("set", true)
+    expect($("#el").attr("class")).to.be("omgYes")
+
+    this.model.set("set", false)
+    expect($("#el").attr("class")).to.be("")
+  })
+
+  it("should handle else replacement values", function() {
+    var Template = new this.TemplateGenerator('<div id="el" class="#{set ? omgYes : omgNo }"></div>').generate()
+      , template = new Template(this.model)
+    
+    $("body").append(template.template)
+        
+    this.model.set("set", false)
+    expect($("#el").attr("class")).to.be("omgNo")
+    
+    this.model.set("set", true)
+    expect($("#el").attr("class")).to.be("omgYes")
+  })
+
 }) 
