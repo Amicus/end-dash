960ea1a7c943bc1c05895fb4b839bc7562068b66
Zach Smith <zach@amicushq.com>
2013-02-12 03:01:43 -0500

    And the cleaning is done

--- test/form_binding.js
+++ test/form_binding.js
@@ -2,19 +2,15 @@ var path = require("path")
   , expect = require("expect.js")
   , fs = require("fs")
   , Backbone = require("backbone")
+  , generateTemplate = require("./util").generateTemplate
 
 describe("when the template has a form element", function() {
   it("should update the model when it changes to it's change", function() {
     var model = new Backbone.Model({ name: "old" })
       , markup = '<div class = "model-"><input type="text" class = "name-"></div>'
-      , TemplateGenerator = window.require("/lib/end-dash")
-      , Template = new TemplateGenerator(markup).generate()
-      , template = new Template({ model: model })
-
-    $("body").html(template.template)
+      , template = generateTemplate({ model: model }, markup)
 
     $(".name-").val("new").change()
-
     expect(model.get("name")).to.be("new")
   })
 })
