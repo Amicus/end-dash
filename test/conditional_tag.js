ce415edecb5a69d5a4f5456b3fed8caaa4be429b
Zach Smith <zach@amicushq.com>
2013-02-12 03:02:51 -0500

    Test hasSyntax-

--- test/conditional_tag.js
+++ test/conditional_tag.js
@@ -12,11 +12,14 @@ describe("A conditional tag", function() {
     var template = generateTemplate(this.model, '<div><div class="hasThing-"></div><div class="isSet-"></div></div>')
 
     expect($(".isSet-").is(":visible")).to.be(false)
+    expect($(".hasThing-").is(":visible")).to.be(false)
 
     this.model.set({ set: true, thing: true })
     expect($(".isSet-").is(":visible")).to.be(true)
+    expect($(".hasThing-").is(":visible")).to.be(true)
 
     this.model.set({ set: false, thing: false })
     expect($(".isSet-").is(":visible")).to.be(false)
+    expect($(".hasThing-").is(":visible")).to.be(false)
   })
 })
