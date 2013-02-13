1af14ed01f952d352dc1b98d21faf347754aa176
Zach Smith <zach@amicushq.com>
2013-02-12 20:24:46 -0500

    data scopes work in all kinds of situations

--- test/scope_modifier.js
+++ test/scope_modifier.js
@@ -3,30 +3,51 @@ var expect = require("expect.js")
   , generateTemplate = require("./util").generateTemplate
   , markup = ''
 
-describe("An embedded model", function() {
-  it("should be able to step out of a scope", function() {
-    var model = { 
-      name: "slash",
-      boot: { 
-        name: "boot",
-        item: {
-          name: "boot-item" 
-        }
-      },
-      root: {
-        name: "root", 
-        thing: { 
-          name: "thing",
-          dude: {
-            name: "dude" 
-          },
-          item: {
-            name: "item" 
+describe("An template", function() {
+  describe("which has data-scope attributes", function() {
+    before(function() {
+      var root = {
+        name: "root",
+        boot: { 
+          name: "boot",
+          sock: {
+            name: "sock" 
           }
-        } 
+        },
+        model: {
+          name: "model", 
+          thing: {
+            name: "thing",
+            dude: {
+              name: "dude" 
+            },
+            item: {
+              name: "item" 
+            }
+          } 
+        }
       }
-    }
-    var template = generateTemplate(model, fs.readFileSync(__dirname + "/support/scopes.html").toString())
-    console.log($("body").html())
+      var template = generateTemplate(root, fs.readFileSync(__dirname + "/support/scopes.html").toString())
+    })
+
+    it("should be able to access the root scope", function() {
+      expect($("#rootName").html()).to.be("root")
+    })
+
+    it("should be able to access child models of the root scope", function() {
+      expect($("#modelName").html()).to.be("model")
+    })
+
+    it("should be able to access a relative scope", function() {
+      expect($("#thingName").html()).to.be("thing")
+    })
+
+    it("should be able to access a model after relative scope", function() {
+      expect($("#itemName").html()).to.be("item")
+    })
+
+    it("should be able to modify the scope of a model", function() {
+      expect($("#sockName").html()).to.be("sock")
+    })
   })
 })
