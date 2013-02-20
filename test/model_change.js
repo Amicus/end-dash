4eb232e4222c1c201a46bdc49c43c1c8fe380620
Zach Smith <zach@amicushq.com>
2013-02-20 16:19:57 -0500

    reload entire child-element-y ness when a model changes.

---
+++ test/model_change.js
@@ -0,0 +1,24 @@
+var generateTemplate = require("./util").generateTemplate
+  , Backbone = require("backbone")
+  , expect = require("expect.js")
+  , _ = require("underscore")
+  , fs = require("fs")
+
+describe("When I replace an embedded model", function() {
+  it("should change it in the template", function() {
+    var subModel = new Backbone.Model({ title: "a title" })
+    var model = new Backbone.Model({ 
+      subModel: subModel
+    })
+      , markup = '<div><div class = "subModel-"><div class = "title-"></div></div></div>'
+      , template = generateTemplate(model, markup)
+  
+    $("body").html(template.template)
+    expect($(".title-").html()).to.be("a title")
+  
+    model.set("subModel", new Backbone.Model({ title: "a new title" }))
+
+    expect($(".title-").html()).to.be("a new title")
+  })
+})
+ 
