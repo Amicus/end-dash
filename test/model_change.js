e2fd7427860702ef9eb7ca2d0aabf94f63ccc626
Zach Smith <zach@amicushq.com>
2013-02-24 19:20:30 -0500

    View requiring fixes and stuff.

--- test/model_change.js
+++ test/model_change.js
@@ -7,20 +7,13 @@ var generateTemplate = require("./util").generateTemplate
 describe("When I replace an embedded model", function() {
   it("should change it in the template", function() {
     var subModel = new Backbone.Model({ title: "a title" })
-    var model = new Backbone.Model({ 
-      subModel: subModel
-    })
+      , model = new Backbone.Model({ subModel: subModel })
       , markup = '<div><div class = "subModel-"><div class = "title-"></div></div></div>'
       , template = generateTemplate(model, markup)
   
-    $("body").html(template.template)
     expect($(".title-").html()).to.be("a title")
   
     model.set("subModel", new Backbone.Model({ title: "a new title" }))
-
-    _(subModel._events).each(function(events, key) {
-      expect(events.length).to.be(0)
-    })
   
     expect($(".title-").html()).to.be("a new title")
   })
