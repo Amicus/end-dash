e77b7a0b8fd4d555e53d9990f9add86c591a40c4
Topper Bowers <topper@toppingdesign.com>
2013-03-08 14:06:13 -0500

    change mocha.opts to not require test/helper.js and change test/helper.js to trigger done after finishing

--- test/helper.js
+++ test/helper.js
@@ -10,11 +10,12 @@ before(function(done) {
   function jsDomLoaded(errors, ctx) {
     global.window = ctx
     global.$ = window.$
-    done() 
     var features = JSON.parse(JSON.stringify(window.document.implementation._features))
 
     window.document.implementation.addFeature('FetchExternalResources', ['script'])
     window.document.implementation.addFeature('ProcessExternalResources', ['script'])
     window.document.implementation.addFeature('MutationEvents', ['1.0'])
+    done()
+
   }
 })
