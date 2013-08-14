cc7dad419842d4a3e2158456f5cb5db09feaaa3f
Zach Smith <zach@amicushq.com>
2013-08-14 17:40:45 -0400

    Live Reloading works; #tooEasy, now for grunt compat

--- test/live-reload.js
+++ test/live-reload.js
@@ -1,18 +1,26 @@
 var path = require("path")
   , expect = require("expect.js")
   , generateTemplate = require("./util").generateTemplate
+  , Model = require("backbone").Model
+  , EndDash = require("../index")
 
 require("../lib/template")
 
 describe("Setting a single variable", function() {
 
   it("should be set in the html", function () {
-    var template = generateTemplate({ singleVariable: "this is value" }, '<div class = "singleVariable-"></div>')
+    var model = new Model({ singleVariable: "this is value", newVariable: "watever" })
+    var template = generateTemplate(model, '<div class = "singleVariable-"></div>')
     expect($(".singleVariable-").html()).to.be("this is value")
 
     var newTemplate = '<div><p class = "singleVariable-"></p><span class="newVariable-"></span></div>'
 
-    var parts = (new EndDash(markup)).serialize()
-    template.init(parts.markup, parts.structure)
+    // this is so ugly! omg.
+    var parts = (new EndDash(newTemplate)).serialize()
+    template.init(parts.structure, parts.markup)
+
+    expect($("p.singleVariable-").html()).to.be("this is value")
+    model.set("singleVariable", "this new value")
+    expect($("p.singleVariable-").html()).to.be("this new value")
   })
 })
