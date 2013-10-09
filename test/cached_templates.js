9d091cc9d0fcba78f453c5e64d225bc5be504d43
Devon Hinton <dhinton@stanford.edu>
2013-10-09 11:21:06 -0400

    Add a second test

--- test/cached_templates.js
+++ test/cached_templates.js
@@ -5,12 +5,18 @@ _ = require('underscore')
 require("./helper")
 
 describe("loading EndDash on the page", function() {
-  it("should store all the scripts of type EndDash as templates", function() {
-    var html = "<script type='EndDash' name='testing'> <div class='test'></div> </script><div class = 'hi'><span></span><span></span><div class = 'woot'></div>"
+  it("should store the scripts of type EndDash as a template", function() {
+    var html = "<script type='EndDash' name='testing'> <div class='test'></div> </script> <div class = 'hi'><span></span><span></span><div class = 'woot'></div>  <script type='EndDash' name='helloWorld'> <div id='baller'> <span></span> </div> <div> Text is here </div> </script>  "
       , $ = window.$
     window.document.body.innerHTML = html
     var EndDash = require('../lib/end-dash')
     expect(EndDash.UnparsedTemplates["testing"]).to.be(" <div class='test'></div> ")
+  })
+
+  it("should store all the scripts of type EndDash as templates", function() {
+    var EndDash = require('../lib/end-dash')
+    expect(EndDash.UnparsedTemplates["testing"]).to.be(" <div class='test'></div> ")
+    expect(EndDash.UnparsedTemplates["helloWorld"]).to.be(" <div id='baller'> <span></span> </div> <div> Text is here </div> ")
   })
 })
 
