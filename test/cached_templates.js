efe6cfa0c24c3c26efc80d4188a1799ac272b752
Devon Hinton <dhinton@stanford.edu>
2013-10-09 15:14:13 -0400

    Failing test that must be explained

--- test/cached_templates.js
+++ test/cached_templates.js
@@ -6,12 +6,12 @@ require("./helper")
 
 describe("loading EndDash on the page", function() {
   it("should store the scripts of type EndDash as a template", function() {
-    var html = "<script type='EndDash' name='testing'> <div class='test'></div> </script> <div class = 'hi'><span></span><span></span><div class = 'woot'></div>  <script type='EndDash' name='helloWorld'> <div id='baller'> <span></span> </div> <div id='test' class='name-'></div> </script>  "
+    var html = '<script type="EndDash" name="testing"> <div class="test"></div> </script> <div class = "hi"><span></span><span></span><div class = "woot"></div>  <script type="EndDash" name="helloWorld"> <div id="baller"> <span></span> </div> <div id="test" class="name-"></div> </script>'
       , $ = window.$
     window.document.body.innerHTML = html
     var EndDash = require('../lib/end-dash')
-    expect(EndDash.rawTemplate("testing")).to.be("<div class='test'></div>")
-    expect(EndDash.rawTemplate("helloWorld")).to.be("<div id='baller'> <span></span> </div> <div id='test' class='name-'></div>")
+    expect(EndDash.rawTemplate("testing")).to.be(" <div class='test'></div> ")
+    expect(EndDash.rawTemplate("helloWorld")).to.be(" <div id='baller'> <span></span> </div> <div id='test' class='name-'></div> ")
   })
 })
 
