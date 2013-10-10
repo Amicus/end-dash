56a28953d8246de916e838bc869852b66c68bf42
Devon Hinton <dhinton@stanford.edu>
2013-10-10 16:05:02 -0400

    wohoooo, it works, now the kinks

--- test/aggregator.js
+++ test/aggregator.js
@@ -1,4 +1,5 @@
 var Aggregator = require('../lib/server_side_dash/aggregator')
+    fs = require('fs')
 
 describe('With a set of nested templates', function(){
   it("should correctly find them, and combine them to a single file", function(){
