df0e72151f70a658e5afef2e691bcf8430d86ad4
Devon Hinton <dhinton@stanford.edu>
2013-10-10 12:04:43 -0400

    Wohooo a few good specs passing

--- test/cached_templates.js
+++ test/cached_templates.js
@@ -2,38 +2,66 @@ _ = require('underscore')
   , expect = require("expect.js")
   , jsdom = require("jsdom")
   , Backbone = require("backbone")
+  , EndDash = require('../lib/end-dash')
 
 require("./helper")
 
-describe("loading EndDash on a page with scripts of type EndDash", function() {
-  it("should cause EndDash to store the templates with the right name", function() {
+describe('With EndDash loaded on a page', function(){
+  beforeEach(function() {
     var html =  '<div> This is the main body </div>' +
-                '<script type="EndDash" name="testing">' +
-                  ' <div class="test"></div>' +
-                '</script>'
-      , $ = window.$
+                  '<script type="EndDash" name="testing">' +
+                    ' <div class="test"></div>' +
+                  '</script>'
+        , $ = window.$
     window.document.body.innerHTML = html
-    var EndDash = require('../lib/end-dash')
-    var endDash = new EndDash()
-    expect(endDash.getHTMLTemplate("testing")).to.be('<div class="test"></div>')
+    this.endDash = new EndDash()
   })
-})
 
-describe("When creating a new template", function() {
-  it("EndDash should return a template with the model passed in bound to the DOM elements", function() {
-    var html = '<script type="EndDash" name="helloWorld">' +
-              ' <div class="name-"></div>' +
-              '</script>'
-  , $ = window.$
-    window.document.body.innerHTML = html
-    var EndDash = require('../lib/end-dash')
-    var endDash = new EndDash()
-    var model = new Backbone.Model({name: "Devon"})
-    var template = endDash.createTemplate("helloWorld", model)
-    $("body").html(template.template)
-    expect($($('.name-')).text()).to.be("Devon")
-    model.set("name", "Brian")
-    expect($($('.name-')).text()).to.be("Brian")
+  describe("loading EndDash on a page with scripts of type EndDash", function() {
+    it("should cause EndDash to store the templates with the right name", function() {
+      expect(this.endDash.getHTMLTemplate("testing")).to.be('<div class="test"></div>')
+    })
   })
+
+  describe("Loading new html onto the page and refreshing EndDash", function() {
+    beforeEach(function() {
+      var html = '<script type="EndDash" name="helloWorld">' +
+                ' <div class="name-"></div>' +
+                '</script>'
+      window.document.body.innerHTML = html
+      this.endDash.reloadTemplates()
+    })
+    it("EndDash should have the new templates but not the old templates", function() {
+      expect(this.endDash.getHTMLTemplate("testing")).to.be(undefined)
+      expect(this.endDash.getHTMLTemplate("helloWorld")).to.be('<div class="name-"></div>')
+    })
+  })
+
+  describe('Binding EndDash to a model on the page should still owrk', function() {
+    beforeEach(function() {
+      var html = '<script type="EndDash" name="helloWorld">' +
+                ' <div class="name-"></div>' +
+                '</script>'
+      window.document.body.innerHTML = html
+      this.endDash.reloadTemplates()
+    })
+    it("EndDash should return a template with the model passed in bound to the DOM elements", function() {
+      var model = new Backbone.Model({name: "Devon"})
+      var template = this.endDash.createTemplate("helloWorld", model)
+      $("body").html(template.template)
+      expect($($('.name-')).text()).to.be("Devon")
+
+      describe("Changing the model", function(){
+        beforeEach(function() {
+          model.set("name", "Brian")
+        })
+        it("The template should reflect the change", function() {
+          expect($($('.name-')).text()).to.be("Brian")
+        })
+      })
+
+    })
+  })
+
 })
 
