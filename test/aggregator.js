5b7a179cabd049ba4979402cfe8bf5be8fdae28f
Devon Hinton <dhinton@stanford.edu>
2013-10-10 15:30:02 -0400

    draft of test and add code to write script tag wrappers for the EndDash templates wohoo

---
+++ test/aggregator.js
@@ -0,0 +1,9 @@
+var Aggregator = require('../lib/server_side_dash/aggregator')
+
+describe('With a set of nested templates', function(){
+  it("should correctly find them, and combine them to a single file", function(){
+
+  })
+
+
+})
