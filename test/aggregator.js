b275f87ffd108ab495d0f83606047e85e4d5193e
Devon Hinton <dhinton@stanford.edu>
2013-10-10 15:50:29 -0400

    for now expect an absolute path to the directory

--- test/aggregator.js
+++ test/aggregator.js
@@ -2,7 +2,7 @@ var Aggregator = require('../lib/server_side_dash/aggregator')
 
 describe('With a set of nested templates', function(){
   it("should correctly find them, and combine them to a single file", function(){
-
+    var aggregator = new Aggregator({templateDir: __dirname + "/support/templates/", serveRoot: __dirname})
   })
 
 
