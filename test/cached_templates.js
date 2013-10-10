d3040753af41f59b9b6d6b1e6ce5ee62dc514fce
Devon Hinton <dhinton@stanford.edu>
2013-10-10 11:46:32 -0400

    A second test passing note, that they must be backbone models and fire events for this to work

--- test/cached_templates.js
+++ test/cached_templates.js
@@ -1,12 +1,14 @@
 _ = require('underscore')
   , expect = require("expect.js")
   , jsdom = require("jsdom")
+  , Backbone = require("backbone")
 
 require("./helper")
 
-describe("loading EndDash on the page", function() {
-  it("should store the scripts of type EndDash as a template", function() {
-    var html = '<script type="EndDash" name="testing">' +
+describe("loading EndDash on a page with scripts of type EndDash", function() {
+  it("should cause EndDash to store the templates with the right name", function() {
+    var html =  '<div> This is the main body </div>' +
+                '<script type="EndDash" name="testing">' +
                   ' <div class="test"></div>' +
                 '</script>'
       , $ = window.$
@@ -17,9 +19,8 @@ describe("loading EndDash on the page", function() {
   })
 })
 
-describe("binding a new model on the page", function() {
-  it("should store the scripts of type EndDash as a template", function() {
-
+describe("When creating a new template", function() {
+  it("EndDash should return a template with the model passed in bound to the DOM elements", function() {
     var html = '<script type="EndDash" name="helloWorld">' +
               ' <div class="name-"></div>' +
               '</script>'
@@ -27,10 +28,12 @@ describe("binding a new model on the page", function() {
     window.document.body.innerHTML = html
     var EndDash = require('../lib/end-dash')
     var endDash = new EndDash()
-    var model = {name: "Devon"}
+    var model = new Backbone.Model({name: "Devon"})
     var template = endDash.createTemplate("helloWorld", model)
     $("body").html(template.template)
     expect($($('.name-')).text()).to.be("Devon")
+    model.set("name", "Brian")
+    expect($($('.name-')).text()).to.be("Brian")
   })
 })
 
