b1ef5fa67e78460f3e3d2ee8830391844338db1c
Devon Hinton <dhinton@stanford.edu>
2013-10-09 20:08:28 -0400

    I said good day JQuery and bad formation of the library

--- test/cached_templates.js
+++ test/cached_templates.js
@@ -6,19 +6,28 @@ require("./helper")
 
 describe("loading EndDash on the page", function() {
   it("should store the scripts of type EndDash as a template", function() {
-    var html = '<script type="EndDash" name="testing"> <div class="test"></div> </script> <div class = "hi"><span></span><span></span><div class = "woot"></div>  <script type="EndDash" name="helloWorld"> <div id="baller"> <span></span> </div> <div id="test" class="name-"></div> </script>'
+    var html = '<script type="EndDash" name="testing">' +
+                  ' <div class="test"></div>' +
+                '</script>'
       , $ = window.$
     window.document.body.innerHTML = html
     var EndDash = require('../lib/end-dash')
-    expect(EndDash.rawTemplate("testing")).to.be(" <div class='test'></div> ")
-    expect(EndDash.rawTemplate("helloWorld")).to.be(" <div id='baller'> <span></span> </div> <div id='test' class='name-'></div> ")
+    var endDash = new EndDash()
+    expect(endDash.getHTMLTemplate("testing")).to.be(" <div class='test'></div> ")
   })
 })
 
 describe("binding a new model on the page", function() {
   it("should store the scripts of type EndDash as a template", function() {
+
+    var html = '<script type="EndDash" name="helloWorld">' +
+              ' <div class="name-"></div>' +
+              '</script>'
+  , $ = window.$
+    window.document.body.innerHTML = html
     var EndDash = require('../lib/end-dash')
-    var template = EndDash.template("helloWorld", {name: "Devon"})
+    var endDash = new EndDash()
+    var template = endDash.createTemplate("helloWorld", {name: "Devon"})
     window.document.body.innerHTML = template.template
     expect($($('#test')).text()).to.be("Devon")
   })
