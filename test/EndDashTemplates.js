e2754647d54c5d5345b4d75aa892f0563a6b770e
Devon Hinton <dhinton@stanford.edu>
2013-10-10 16:12:25 -0400

    cleaner file names

---
+++ test/EndDashTemplates.js
@@ -0,0 +1,24 @@
+
+ <script type='EndDash' name='homepage/intro_content' ><div class="welcomeContent">
+  <span class="mainline"> Welcome! </span>
+</div>
+ </script>
+ <script type='EndDash' name='ordered_list' ><ol class="interactions-">
+  <li class="interaction- isNotDismissed-">
+    <div class="user-">
+    </div>
+    <span class="isRespondable-">
+      <button class="respond responseText- hasResponseText- submit communityBtn"></button>
+    </span>
+    <ul class="interactionResponses-">
+      <li class="interactionResponse-">
+        <div class="responseIcon"></div>
+        <div class="content-"></div>
+      </li>
+    </ul>
+  </li>
+</ol>
+ </script>
+ <script type='EndDash' name='show' ><div class="name-"></div>
+<div id="while"></div>
+ </script>
