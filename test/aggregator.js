9f855d558a8090cfcf5d9b63c870a84f672d849c
Devon Hinton <dhinton@stanford.edu>
2013-10-10 16:21:20 -0400

    Wooot, will now clean up old template file if it exists

--- test/aggregator.js
+++ test/aggregator.js
@@ -2,7 +2,9 @@ var Aggregator = require('../lib/server_side_dash/aggregator')
     fs = require('fs')
 
 describe('With a set of nested templates', function(){
+
   it("should correctly find them, and combine them to a single file", function(){
+
     var aggregator = new Aggregator({templateDir: __dirname + "/support/templates/", serveRoot: __dirname})
   })
 
