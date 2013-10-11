74d8ad18822330d1b338e1576fb3ab133e9550d6
Devon Hinton <dhinton@stanford.edu>
2013-10-11 11:14:29 -0400

    An integrative test

---
+++ test/new/new_end-dash.js
@@ -0,0 +1,39 @@
+var expect = require("expect.js")
+  , jsdom = require("jsdom")
+  , Backbone = require("backbone")
+  , EndDash = require('../../lib/end-dash')
+  , Aggregator = require('../../lib/server_side_dash/aggregator.js')
+  , fs = require('fs')
+
+require("../helper")
+_ = require('underscore') //Global for EndDash
+
+
+
+describe("With a set of templates in a directory aggregated to one file", function(){
+  beforeEach(function(){
+    var aggregator = new Aggregator({templateDir: __dirname + "/../support/templates/", serveRoot: __dirname})
+    aggregator.loadFiles()
+  })
+  afterEach(function(){
+    fs.unlinkSync(__dirname + '/EndDashTemplates.js')
+  })
+
+  describe("With a web page with EndDash and the templates loaded", function(){
+
+    beforeEach(function(){
+      var html = fs.readFileSync(__dirname + "/EndDashTemplates.js", 'utf8')
+        , $ = window.$
+      window.document.body.innerHTML = html
+      this.endDash = new EndDash()
+    })
+
+    it("should load the templates into EndDash", function(){
+      expect(this.endDash.getHTMLTemplate("homepage/intro_content")).not.to.be(undefined)
+      expect(this.endDash.getHTMLTemplate("ordered_list")).not.to.be(undefined)
+      expect(this.endDash.getHTMLTemplate("show")).not.to.be(undefined)
+    })
+
+  })
+
+})
