d06b7025821f88d8feb325a7f5ed41c17b6979bd
Devon Hinton <dhinton@stanford.edu>
2013-10-15 17:22:58 -0400

    Add moar specs!

--- test/new/new_end-dash.js
+++ test/new/new_end-dash.js
@@ -19,20 +19,36 @@ describe("With a set of templates loaded into EndDash", function(){
     EndDash.registerTemplate("list_item.html", list_item)
     EndDash.registerTemplate("scopes", scopes)
   })
+
   it("should have all the teamples interanlly", function(){
     expect(EndDash.isTemplateLoaded("partials")).to.be(true)
     expect(EndDash.isTemplateLoaded("embedded_partial.html")).to.be(true)
     expect(EndDash.isTemplateLoaded("list_item.html")).to.be(true)
     expect(EndDash.isTemplateLoaded("scopes")).to.be(true)
   })
-  it("binding templates should work", function(){
-    var items = new Backbone.Collection()
-    var item1 = new Backbone.Model({variable: "Candy"})
-    var item2 = new Backbone.Model({variable: "Cane"})
-    var item3 = new Backbone.Model({variable: "Good"})
-    items.add(item1).add(item2).add(item3)
-    var root = new Backbone.Model({items: items, thing: {name: "very"}})
-    EndDash.bindTemplate("partials", root)
+
+  describe("Binding a template on the page", function(){
+    beforeEach(function(){
+      this.items = new Backbone.Collection()
+      this.item1 = new Backbone.Model({variable: "Candy"})
+      this.item2 = new Backbone.Model({variable: "Cane"})
+      this.item3 = new Backbone.Model({variable: "Good"})
+      this.items.add(this.item1).add(this.item2).add(this.item3)
+      this.root = new Backbone.Model({items: this.items, thing: {name: "very"}})
+      this.template = EndDash.bindTemplate("partials", this.root)
+    })
+    it("EndDash should fill in the model values in the template", function(){
+      expect($('.item-:nth-child(1) div', this.template.el).text()).to.be("Candy")
+      expect($('.item-:nth-child(2) div', this.template.el).text()).to.be("Cane")
+      expect($('.item-:nth-child(3) div', this.template.el).text()).to.be("Good")
+      expect($('.name-', this.template.el).text()).to.be("very")
+    })
+    it("Should update when a model changes", function(){
+      this.item1.set("variable", "Steak")
+      expect($('.item-:nth-child(1) div', this.template.el).text()).to.be("Steak")
+      this.item2.set("variable", "Also")
+      expect($('.item-:nth-child(2) div', this.template.el).text()).to.be("Also")
+    })
   })
 })
 
