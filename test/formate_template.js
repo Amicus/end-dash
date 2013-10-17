68ef13cac492dacc5c582d1a95f63744f735b03b
Devon Hinton <dhinton@stanford.edu>
2013-10-17 11:33:02 -0400

    Fix spec for this branch and add a larger test for the templates aggregation module

--- test/formate_template.js
+++ test/formate_template.js
@@ -2,13 +2,12 @@ var formate_templates = require('../lib/formate_templates')
   , fs = require('fs')
   , expect = require('expect.js')
 
-    require('./helper')
+    require('./support/helper')
 
 describe('With a templates directory with two EndDash templates', function(){
     beforeEach(function(done){
-      formate_templates(__dirname + "/support/templates/", __dirname, done)
+      formate_templates(__dirname + "/support/small_template_directory/", __dirname, done)
     })
-
     afterEach(function(){
       fs.unlinkSync(__dirname + '/end_dash_templates.js')
     })
@@ -21,6 +20,23 @@ describe('With a templates directory with two EndDash templates', function(){
       }
       expect(endDash_templates.length).to.be(2)
     })
-
-
 })
+
+describe('With a large templates directory ', function(){
+  beforeEach(function(done){
+    formate_templates(__dirname + "/support", __dirname, done)
+  })
+  afterEach(function(){
+    fs.unlinkSync(__dirname + "/end_dash_templates.js")
+  })
+
+  it("should recursively aggregate all files ending in .html or .js.ed into a single templates file", function(){
+    expect(fs.existsSync(__dirname + "/end_dash_templates.js")).to.be(true)
+    var endDash_templates = fs.readFileSync(__dirname + "/end_dash_templates.js", 'utf8').split("</script>")
+    if (endDash_templates[endDash_templates.length -1] == '\n') { // Template creation finishes with an extra \n
+      endDash_templates.pop()
+    }
+    expect(endDash_templates.length).to.be(15)
+  })
+})
+
