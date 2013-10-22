b01913b2aa034c7ef67e8e568afb7d6f663f29ce
Mukund Lakshman <yaymukund@gmail.com>
2013-10-22 15:53:16 -0400

    Move all test templates to test/support/templates to clean up.

--- test/enumerables.js
+++ test/enumerables.js
@@ -8,7 +8,7 @@ describe("An enumerable template", function() {
 
   it("should set all the values in the html", function () {
     var model = { people: [{name: "Zach"}, {name: "Dog"}] }
-      , template = generateTemplate(model, fs.readFileSync(__dirname + "/support/enumerable.html").toString())
+      , template = generateTemplate(model, fs.readFileSync(__dirname + "/support/templates/enumerable.html").toString())
 
     expect($(".people- .person-:nth-child(1) .name-").html()).to.be("Zach")
     expect($(".people- .person-:nth-child(2) .name-").html()).to.be("Dog")
@@ -16,7 +16,7 @@ describe("An enumerable template", function() {
 
   it("should make the collection empty", function () {
     var model = { people: [] }
-      , template = generateTemplate(model, fs.readFileSync(__dirname + "/support/enumerable.html").toString())
+      , template = generateTemplate(model, fs.readFileSync(__dirname + "/support/templates/enumerable.html").toString())
 
     expect($(".people-").children().length).to.be(0)
   })
