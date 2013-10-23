38f415f7eda29883f368dc44fce32bf5b68a39b4
Devon Hinton <dhinton@stanford.edu>
2013-10-23 14:33:51 -0400

    Semi-colon-ized and template string split to multiple lines for greater readability

--- test/non_backbone_integration.js
+++ test/non_backbone_integration.js
@@ -4,60 +4,60 @@ var path = require("path")
   , expect = require("expect.js")
   , fs = require("fs")
   , generateTemplate = require("./support/generate_template")
-  , Backbone = require('Backbone')
+  , Backbone = require('Backbone');
 
-delete require.cache[require.resolve('Backbone')]
-var BackboneClone = require('Backbone') // Internally we use !(model instanceof Backbone.Model || ... instanceof Backbone.Collection)
+delete require.cache[require.resolve('Backbone')];
+var BackboneClone = require('Backbone');// Internally we use !(model instanceof Backbone.Model || ... instanceof Backbone.Collection)
                                         // If the Backbone for EndDash is different then the clients, we want to handle this case.
                                         // See lib/reactions/model.js for fix.
 
 describe("With a backbone model from a different backbone object", function(){
   it("binding it to a template should work normally", function(){
     var model = new BackboneClone.Model({persisted: "Chelsa Piers"})
-      , template = generateTemplate({user: model}, "<div class='user-'><div class='persisted-'></div></div>")
+      , template = generateTemplate({user: model}, "<div class='user-'><div class='persisted-'></div></div>");
 
-    expect($('.persisted-').html()).to.be('Chelsa Piers')
-    model.set('persisted', 'Changed')
-    expect($('.persisted-').html()).to.be('Changed')
+    expect($('.persisted-').html()).to.be('Chelsa Piers');
+    model.set('persisted', 'Changed');
+    expect($('.persisted-').html()).to.be('Changed');
   })
 })
 
 describe("With a backbone model", function(){
   beforeEach(function(){
-    var backModel = new Backbone.Model()
+    var backModel = new Backbone.Model();
   })
   it("binding it to a template should work normally", function(){
     var model = new Backbone.Model({persisted: "Chelsa Piers"})
-      , template = generateTemplate({user: model}, "<div class='user-'><div class='persisted-'></div></div>")
+      , template = generateTemplate({user: model}, "<div class='user-'><div class='persisted-'></div></div>");
 
-    expect($('.persisted-').html()).to.be('Chelsa Piers')
-    model.set('persisted', 'Changed')
-    expect($('.persisted-').html()).to.be('Changed')
+    expect($('.persisted-').html()).to.be('Chelsa Piers');
+    model.set('persisted', 'Changed');
+    expect($('.persisted-').html()).to.be('Changed');
   })
 })
 
 describe("With a backbone collection from a different backbone object", function(){
   beforeEach(function(){
-    this.collection = new BackboneClone.Collection()
-    this.templateString = "<div class='users-'><div class ='user-'><div class=' #{hook} persisted-'></div></div></div>"
+    this.collection = new BackboneClone.Collection();
+    this.templateString = "<div class='users-'><div class ='user-'><div class=' #{hook} persisted-'></div></div></div>";
   })
 
   describe("With models from a different backbone object", function(){
     it("binding the collection and models to a template should work normally", function(){
       var model1 = new BackboneClone.Model({persisted: "Chelsa Piers", hook: '1'})
         , model2 = new BackboneClone.Model({persisted: "Columbus circle", hook: '2'})
-        , model3 = new BackboneClone.Model({persisted: "Soho", hook: '3'})
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be(model2.get('persisted'))
-      expect($('.3').html()).to.be(model3.get('persisted'))
-      model1.set('persisted', 'Changed1')
-      model2.set('persisted', 'Changed2')
-      model3.set('persisted', 'Changed3')
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be(model2.get('persisted'))
-      expect($('.3').html()).to.be(model3.get('persisted'))
+        , model3 = new BackboneClone.Model({persisted: "Soho", hook: '3'});
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be(model2.get('persisted'));
+      expect($('.3').html()).to.be(model3.get('persisted'));
+      model1.set('persisted', 'Changed1');
+      model2.set('persisted', 'Changed2');
+      model3.set('persisted', 'Changed3');
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be(model2.get('persisted'));
+      expect($('.3').html()).to.be(model3.get('persisted'));
     })
   })
 
@@ -65,18 +65,18 @@ describe("With a backbone collection from a different backbone object", function
     it("should interpolate values but not update on model changes", function(){
       var model1 = {persisted: "Chelsa Piers", hook: '1'}
         , model2 = {persisted: "Columbus circle", hook: '2'}
-        , model3 = {persisted: "Soho", hook: '3'}
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(model1.persisted)
-      expect($('.2').html()).to.be(model2.persisted)
-      expect($('.3').html()).to.be(model3.persisted)
-      model1.persisted = 'Changed1'
-      model2.persisted = 'Changed2'
-      model3.persisted = 'Changed3'
-      expect($('.1').html()).to.be("Chelsa Piers")
-      expect($('.2').html()).to.be("Columbus circle")
-      expect($('.3').html()).to.be("Soho")
+        , model3 = {persisted: "Soho", hook: '3'};
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(model1.persisted);
+      expect($('.2').html()).to.be(model2.persisted);
+      expect($('.3').html()).to.be(model3.persisted);
+      model1.persisted = 'Changed1';
+      model2.persisted = 'Changed2';
+      model3.persisted = 'Changed3';
+      expect($('.1').html()).to.be("Chelsa Piers");
+      expect($('.2').html()).to.be("Columbus circle");
+      expect($('.3').html()).to.be("Soho");
     })
   })
 
@@ -84,44 +84,49 @@ describe("With a backbone collection from a different backbone object", function
     it("binding them to a template should still work for each as expected", function(){
       var model1 = new BackboneClone.Model({persisted: "Chelsa Piers", hook: '1'})
         , model2 = {persisted: "Columbus circle", hook: '2'}
-        , model3 = new Backbone.Model({persisted: "Soho", hook: '3'})
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be(model2.persisted)
-      expect($('.3').html()).to.be(undefined)
-      model1.set('persisted', 'Changed1')
-      model2.persisted = 'Changed2'
-      model3.set('persisted', 'Changed3')
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be("Columbus circle")
-      expect($('.3').html()).to.be(undefined)
+        , model3 = new Backbone.Model({persisted: "Soho", hook: '3'});
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be(model2.persisted);
+      expect($('.3').html()).to.be(undefined);
+      model1.set('persisted', 'Changed1');
+      model2.persisted = 'Changed2';
+      model3.set('persisted', 'Changed3');
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be("Columbus circle");
+      expect($('.3').html()).to.be(undefined);
     })
   })
 })
 
 describe("With a backbone collection from the same backbone object", function(){
   beforeEach(function(){
-    this.collection = new Backbone.Collection()
-    this.templateString = "<div class='users-'><div class ='user-'><div class=' #{hook} persisted-'></div></div></div>"
+    this.collection = new Backbone.Collection();
+    this.templateString = "<div class='users-'>" +
+                            "<div class ='user-'>" +
+                              "<div class=' #{hook} persisted-'>" +
+                              "</div>" +
+                            "</div>" +
+                          "</div>";
   })
 
   describe("With models from a different backbone object", function(){
     it("Binding them to a template should not work properly", function(){
       var model1 = new BackboneClone.Model({persisted: "Chelsa Piers", hook: '1'})
         , model2 = new BackboneClone.Model({persisted: "Columbus circle", hook: '2'})
-        , model3 = new BackboneClone.Model({persisted: "Soho", hook: '3'})
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(undefined)
-      expect($('.2').html()).to.be(undefined)
-      expect($('.3').html()).to.be(undefined)
-      model1.set('persisted', 'Changed1')
-      model2.set('persisted', 'Changed2')
-      model3.set('persisted', 'Changed3')
-      expect($('.1').html()).to.be(undefined)
-      expect($('.2').html()).to.be(undefined)
-      expect($('.3').html()).to.be(undefined)
+        , model3 = new BackboneClone.Model({persisted: "Soho", hook: '3'});
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(undefined);
+      expect($('.2').html()).to.be(undefined);
+      expect($('.3').html()).to.be(undefined);
+      model1.set('persisted', 'Changed1');
+      model2.set('persisted', 'Changed2');
+      model3.set('persisted', 'Changed3');
+      expect($('.1').html()).to.be(undefined);
+      expect($('.2').html()).to.be(undefined);
+      expect($('.3').html()).to.be(undefined);
     })
   })
 
@@ -129,18 +134,18 @@ describe("With a backbone collection from the same backbone object", function(){
     it("binding them to a template should work normally", function(){
       var model1 = new Backbone.Model({persisted: "Chelsa Piers", hook: '1'})
         , model2 = new Backbone.Model({persisted: "Columbus circle", hook: '2'})
-        , model3 = new Backbone.Model({persisted: "Soho", hook: '3'})
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be(model2.get('persisted'))
-      expect($('.3').html()).to.be(model3.get('persisted'))
-      model1.set('persisted', 'Changed1')
-      model2.set('persisted', 'Changed2')
-      model3.set('persisted', 'Changed3')
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be(model2.get('persisted'))
-      expect($('.3').html()).to.be(model3.get('persisted'))
+        , model3 = new Backbone.Model({persisted: "Soho", hook: '3'});
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be(model2.get('persisted'));
+      expect($('.3').html()).to.be(model3.get('persisted'));
+      model1.set('persisted', 'Changed1');
+      model2.set('persisted', 'Changed2');
+      model3.set('persisted', 'Changed3');
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be(model2.get('persisted'));
+      expect($('.3').html()).to.be(model3.get('persisted'));
     })
   })
 
@@ -148,18 +153,18 @@ describe("With a backbone collection from the same backbone object", function(){
     it("should still interpolate values but changes will not be updated", function(){
       var model1 = {persisted: "Chelsa Piers", hook: '1'}
         , model2 = {persisted: "Columbus circle", hook: '2'}
-        , model3 = {persisted: "Soho", hook: '3'}
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(model1.persisted)
-      expect($('.2').html()).to.be(model2.persisted)
-      expect($('.3').html()).to.be(model3.persisted)
-      model1.persisted = 'Changed1'
-      model2.persisted = 'Changed2'
-      model3.persisted = 'Changed3'
-      expect($('.1').html()).to.be("Chelsa Piers")
-      expect($('.2').html()).to.be("Columbus circle")
-      expect($('.3').html()).to.be("Soho")
+        , model3 = {persisted: "Soho", hook: '3'};
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(model1.persisted);
+      expect($('.2').html()).to.be(model2.persisted);
+      expect($('.3').html()).to.be(model3.persisted);
+      model1.persisted = 'Changed1';
+      model2.persisted = 'Changed2';
+      model3.persisted = 'Changed3';
+      expect($('.1').html()).to.be("Chelsa Piers");
+      expect($('.2').html()).to.be("Columbus circle");
+      expect($('.3').html()).to.be("Soho");
     })
   })
 
@@ -167,18 +172,18 @@ describe("With a backbone collection from the same backbone object", function(){
     it("binding them to a template should still work for each type as expected", function(){
       var model1 = new Backbone.Model({persisted: "Chelsa Piers", hook: '1'})
         , model2 = {persisted: "Columbus circle", hook: '2'}
-        , model3 = new BackboneClone.Model({persisted: "Soho", hook: '3'})
-      this.collection.add([model1, model2, model3])
-      var template = generateTemplate({users: this.collection}, this.templateString)
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be(model2.persisted)
-      expect($('.3').html()).to.be(undefined)
-      model1.set('persisted', 'Changed1')
-      model2.persisted = 'Changed2'
-      model3.set('persisted', 'Changed3')
-      expect($('.1').html()).to.be(model1.get('persisted'))
-      expect($('.2').html()).to.be("Columbus circle")
-      expect($('.3').html()).to.be(undefined)
+        , model3 = new BackboneClone.Model({persisted: "Soho", hook: '3'});
+      this.collection.add([model1, model2, model3]);
+      var template = generateTemplate({users: this.collection}, this.templateString);
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be(model2.persisted);
+      expect($('.3').html()).to.be(undefined);
+      model1.set('persisted', 'Changed1');
+      model2.persisted = 'Changed2';
+      model3.set('persisted', 'Changed3');
+      expect($('.1').html()).to.be(model1.get('persisted'));
+      expect($('.2').html()).to.be("Columbus circle");
+      expect($('.3').html()).to.be(undefined);
     })
   })
 })
