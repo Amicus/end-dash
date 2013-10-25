17bd21d8c01d18a451281089fecc03203c0875bd
Brian Glusman <brian@amicushq.com>
2013-10-25 14:23:42 -0400

    move rootElements method to parser; refactor test.

--- test/template_root_elements.js
+++ test/template_root_elements.js
@@ -1,28 +1,27 @@
 var expect = require("expect.js"),
-    TemplateStore = require('../lib/template_store'),
+    Parser = require('../lib/parser')
 
     // easier to read..
-    load = TemplateStore.load,
-    rootElements = TemplateStore.rootElements;
+    rootElements = Parser.rootElements,
+    singleElemenetMarkup = '<div id="user"></div>',
+    secondElementMarkup  = '<div id="membership"></div>'
+    multiElementMarkup  = singleElemenetMarkup + secondElementMarkup;
+
 
 describe('rootElements', function() {
   it('parses the markup and returns each root element as a separate string', function() {
     describe('with a single root element', function() {
-      load('singleRoot', '<div id="user"></div>');
-      expect(rootElements('singleRoot').length).to.be(1);
+      expect(rootElements(singleElemenetMarkup).length).to.be(1);
     });
     describe('array response of a single root element', function() {
-      load('singleRoot', '<div id="user"></div>');
-      expect(rootElements('singleRoot')[0]).to.be('<div id="user"></div>');
+      expect(rootElements(singleElemenetMarkup)[0]).to.be(singleElemenetMarkup);
     });
     describe('with multiple root elements', function() {
-      load('singleRoot', '<div id="user"></div><div id="membership"></div>');
-      expect(rootElements('singleRoot').length).to.be(2);
+      expect(rootElements(multiElementMarkup).length).to.be(2);
     });
     describe('array response of a multiple root element', function() {
-      load('singleRoot', '<div id="user"></div><div id="membership"></div>');
-      expect(rootElements('singleRoot')[0]).to.be('<div id="user"></div>');
-      expect(rootElements('singleRoot')[1]).to.be('<div id="membership"></div>');
+      expect(rootElements(multiElementMarkup)[0]).to.be(singleElemenetMarkup);
+      expect(rootElements(multiElementMarkup)[1]).to.be(secondElementMarkup);
     });
   });
 });
