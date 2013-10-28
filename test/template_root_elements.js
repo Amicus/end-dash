934073be6bd62a410fbd2ec36d105e9390994b1f
Brian Glusman <brian@amicushq.com>
2013-10-28 10:04:28 -0400

    simple error on multiple roots

---
+++ test/template_root_elements.js
@@ -0,0 +1,17 @@
+var expect = require("expect.js"),
+    Parser = require('../lib/parser')
+
+    // easier to read..
+    singleElemenetMarkup = '<div id="user"></div>',
+    secondElementMarkup  = '<div id="membership"></div>'
+    multiElementMarkup  = singleElemenetMarkup + secondElementMarkup;
+
+
+describe('multiple rootElements', function() {
+  it('errors if multiple root elements are present by default', function() {
+    multiRootParse = function() {
+      new Parser(multiElementMarkup, {templateName: 'test',templates: {}})
+    }
+    expect(multiRootParse).to.throwError(/multiple root elements/);
+  });
+});
