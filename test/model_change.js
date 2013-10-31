62043914c5e3adc10aeec46ce5ec125322d736cd
Devon Hinton <dhinton@stanford.edu>
2013-10-31 14:21:30 -0400

    Test for variable reactions not being created for polymorphic keys or values

--- test/model_change.js
+++ test/model_change.js
@@ -19,31 +19,31 @@ describe("When I replace an embedded model", function() {
       , model = new Backbone.Model({ subModel: subModel })
       , markup = '<div><div class = "subModel-"><div class = "title-"></div></div></div>'
       , template = generateTemplate(model, markup)
-  
+
     expect($(".title-").html()).to.be("a title")
-  
+
     model.set("subModel", new Backbone.Model({ title: "a new title" }))
-  
+
     expect($(".title-").html()).to.be("a new title")
   })
 
   it("should stop listening to old views", function(done) {
     views.testView = function() {
       var once
-      this.stopListening = this.undelegateEvents = function() { 
-        once = (once) ? done() : true 
-      } 
+      this.stopListening = this.undelegateEvents = function() {
+        once = (once) ? done() : true
+      }
     }
     var subModel = new Backbone.Model({ title: "a title" })
       , model = new Backbone.Model({ subModel: subModel })
       , markup = '<div><div class = "subModel-" data-view="testView"><div class = "title-"></div></div></div>'
       , template = generateTemplate(model, markup)
 
-  
+
     expect($(".title-").html()).to.be("a title")
-  
+
     model.set("subModel", new Backbone.Model({ title: "a new title" }))
-  
+
     expect($(".title-").html()).to.be("a new title")
   })
 })
