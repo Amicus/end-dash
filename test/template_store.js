68eef8792e2c980c895ae62813b3714ba85ef460
Devon Hinton <dhinton@stanford.edu>
2013-10-31 17:41:46 -0400

    Template store failing spec

--- test/template_store.js
+++ test/template_store.js
@@ -9,8 +9,7 @@ var expect = require("expect.js"),
     isLoaded = TemplateStore.isLoaded,
     isParsed = TemplateStore.isParsed,
     load = TemplateStore.load,
-    loadAndParse = TemplateStore.loadAndParse,
-    getTemplateClass = TemplateStore.getTemplateClass;
+    loadParsed = TemplateStore.loadParsed;
 
 describe('TemplateStore', function() {
   describe('.load', function() {
@@ -21,62 +20,69 @@ describe('TemplateStore', function() {
     });
   });
 
-  describe('.getTemplateClass', function() {
-    it('retrieves loaded templates', function() {
-      load('user', '<div id="user"></div>');
-      var UserTemplate = getTemplateClass('user');
-      expect(UserTemplate).to.be.a(Template.constructor);
-    });
-
-    it('returns the same Template object when called repeatedly', function() {
-      load('user', '<div id="user"></div>');
-      var UserTemplate = getTemplateClass('user'),
-          UserTemplate2 = getTemplateClass('user');
-
-      expect(UserTemplate2).to.equal(UserTemplate);
-    });
-
-    it('retrieves templates using a normalized path', function() {
-      var BarTemplate = loadAndParse('/foo/bar', '<div id="bar"></div>');
-
-      expect(getTemplateClass('/foo/bar')).to.equal(BarTemplate);
-      expect(getTemplateClass('/foo/bar/baz/..')).to.equal(BarTemplate);
-      expect(getTemplateClass('/foo/baz/../bar')).to.equal(BarTemplate);
-    });
-
-    it('errors when it can\'t find a template', function() {
-      expect(function() {
-        getTemplateClass('notLoaded')
-      }).to.throwError(/Could not find template/);
-    });
-
-    it('parses templates that haven\'t been parsed', function() {
-      load('user', '<div id="user"></div>');
-
-      expect(isParsed('user')).to.be(false);
-      getTemplateClass('user');
-      expect(isParsed('user')).to.be(true);
+  describe('.loadParsed', function(){
+    it('loads the parsed template', function(){
+      loadParsed('user', 'blah');
+      expect(isParsed).to.be(true);
     });
   });
 
-  describe('.loadAndParse', function() {
-    it('loads and parses', function() {
-      loadAndParse('user', '<div id="user"></div>');
+  // describe('.getTemplateClass', function() {
+  //   it('retrieves loaded templates', function() {
+  //     load('user', '<div id="user"></div>');
+  //     var UserTemplate = getTemplateClass('user');
+  //     expect(UserTemplate).to.be.a(Template.constructor);
+  //   });
 
-      expect(isLoaded('user')).to.be(true);
-      expect(isParsed('user')).to.be(true);
-    });
+  //   it('returns the same Template object when called repeatedly', function() {
+  //     load('user', '<div id="user"></div>');
+  //     var UserTemplate = getTemplateClass('user'),
+  //         UserTemplate2 = getTemplateClass('user');
 
-    it('returns a Template object', function() {
-      var UserTemplate = loadAndParse('user', '<div id="user"></div>');
-      expect(UserTemplate).to.be.a(Template.constructor);
-    });
+  //     expect(UserTemplate2).to.equal(UserTemplate);
+  //   });
 
-    it('overwrites loaded templates', function() {
-      var UserTemplate = loadAndParse('user', '<div id="user"></div>'),
-          UserTemplate2 = loadAndParse('user', '<div id="user"></div>');
+  //   it('retrieves templates using a normalized path', function() {
+  //     var BarTemplate = loadAndParse('/foo/bar', '<div id="bar"></div>');
 
-      expect(UserTemplate2).to.not.equal(UserTemplate);
-    });
-  });
+  //     expect(getTemplateClass('/foo/bar')).to.equal(BarTemplate);
+  //     expect(getTemplateClass('/foo/bar/baz/..')).to.equal(BarTemplate);
+  //     expect(getTemplateClass('/foo/baz/../bar')).to.equal(BarTemplate);
+  //   });
+
+  //   it('errors when it can\'t find a template', function() {
+  //     expect(function() {
+  //       getTemplateClass('notLoaded')
+  //     }).to.throwError(/Could not find template/);
+  //   });
+
+  //   it('parses templates that haven\'t been parsed', function() {
+  //     load('user', '<div id="user"></div>');
+
+  //     expect(isParsed('user')).to.be(false);
+  //     getTemplateClass('user');
+  //     expect(isParsed('user')).to.be(true);
+  //   });
+  // });
+
+  // describe('.loadAndParse', function() {
+  //   it('loads and parses', function() {
+  //     loadAndParse('user', '<div id="user"></div>');
+
+  //     expect(isLoaded('user')).to.be(true);
+  //     expect(isParsed('user')).to.be(true);
+  //   });
+
+  //   it('returns a Template object', function() {
+  //     var UserTemplate = loadAndParse('user', '<div id="user"></div>');
+  //     expect(UserTemplate).to.be.a(Template.constructor);
+  //   });
+
+  //   it('overwrites loaded templates', function() {
+  //     var UserTemplate = loadAndParse('user', '<div id="user"></div>'),
+  //         UserTemplate2 = loadAndParse('user', '<div id="user"></div>');
+
+  //     expect(UserTemplate2).to.not.equal(UserTemplate);
+  //   });
+  // });
 });
