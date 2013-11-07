faa6e90b7f3d243154b35d81b30a8a85a56bcac0
Devon Hinton <dhinton@stanford.edu>
2013-11-07 11:57:17 -0500

    test/template_root_element

--- test/template_root_elements.js
+++ test/template_root_elements.js
@@ -1,16 +1,14 @@
 var expect = require("expect.js"),
-    Parser = require('../lib/parser')
-
-    // easier to read..
+    Parser = require('../lib/parser'),
     singleElemenetMarkup = '<div id="user"></div>',
-    secondElementMarkup  = '<div id="membership"></div>'
+    secondElementMarkup  = '<div id="membership"></div>',
     multiElementMarkup  = singleElemenetMarkup + secondElementMarkup;
 
 
 describe('multiple rootElements', function() {
   it('errors if multiple root elements are present by default', function() {
     multiRootParse = function() {
-      new Parser(multiElementMarkup, {templateName: 'test',templates: {}})
+      new Parser(multiElementMarkup, {templateName: 'test',templates: {}});
     }
     expect(multiRootParse).to.throwError(/invalid/);
   });
