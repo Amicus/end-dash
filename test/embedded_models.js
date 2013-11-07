c142772fe7946444fb80194da22b17c4daf2f260
Devon Hinton <dhinton@stanford.edu>
2013-11-07 11:19:40 -0500

    test/embedded_models

--- test/embedded_models.js
+++ test/embedded_models.js
@@ -1,15 +1,15 @@
 require('./support/helper');
 
-var path = require("path")
-  , expect = require("expect.js")
-  , fs = require("fs")
-  , generateTemplate = require("./support/generate_template")
+var path = require("path"),
+    expect = require("expect.js"),
+    fs = require("fs"),
+    generateTemplate = require("./support/generate_template");
 
 describe("An embedded model", function() {
   it("should set all the values in the html", function () {
-    var template = generateTemplate({ survey: { name: "A Survey", person: { name: "Zach" } } }, fs.readFileSync(__dirname + "/support/templates/embedded_models.html").toString())
+    var template = generateTemplate({ survey: { name: "A Survey", person: { name: "Zach" } } }, fs.readFileSync(__dirname + "/support/templates/embedded_models.html").toString());
 
-    expect($("#surveyName").html()).to.be("A Survey")
-    expect($(".person- .name-").html()).to.be("Zach")
-  })
-})
+    expect($("#surveyName").html()).to.be("A Survey");
+    expect($(".person- .name-").html()).to.be("Zach");
+  });
+});
