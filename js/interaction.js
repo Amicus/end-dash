9730f81c0f4a276813c61f58d090600f3bc9170d
Brian Glusman <brian@amicushq.com>
2013-11-12 11:37:16 -0500

    subNav hide reveal and move js into doc.ready

--- js/interaction.js
+++ js/interaction.js
@@ -35,42 +35,50 @@ $(document).ready(function() {
     })
   }
   output.hide().html(html).show('slow');
+  $('.container').find('a').addClass('hidden');
+  $('ol').siblings().siblings().addClass('hidden');
+  $('li').on('click', function(evt) {
+    if ($($(evt.target).parents()[2]).hasClass('toc')) {
+      $('ol').siblings().siblings().addClass('hidden');
+      $(evt.target).siblings().removeClass('hidden');
+      $(evt.target).siblings().children().addClass('subNav');
+    }
+  });
+
+  //interactive template example
+  EndDash.bootstrap();
+  var render = function(){
+    try {
+      var modelText = $('#model-edit').text(),
+          templateText = $('#template-edit').text();
+      EndDash.registerTemplate('custom', templateText);
+
+      var template = EndDash.getTemplate('custom', eval(modelText));
+      $('#user-edit').html(template.el);
+    }
+    catch(error) {
+       console.log("Error occurred:", error)
+    }
+  };
+  render();
+  $('#model-edit').on('change keyup keydown', render);
+  $('#template-edit').on('change keyup keydown', render);
+
+  //interactive inputs example
+  // Load all the templates on the page.
+
+  var you = new Backbone.Model({
+    name: 'YourName',
+  });
+
+  var extremeCharacter = new Backbone.Model({
+    name: 'Tony'
+  });
+
+  var template = EndDash.getTemplate('inputName', you),
+      template2 = EndDash.getTemplate('whichCharacter', extremeCharacter);
+
+  $('#inputs').html(template.el);
+  $('#inputs2').html(template2.el);
+
 });
-
-$('.container').find('a').addClass('hidden');
-
-//interactive template example
-EndDash.bootstrap();
-var render = function(){
-  try {
-    var modelText = $('#model-edit').text(),
-        templateText = $('#template-edit').text();
-    EndDash.registerTemplate('custom', templateText);
-
-    var template = EndDash.getTemplate('custom', eval(modelText));
-    $('#user-edit').html(template.el);
-  }
-  catch(error) {
-     console.log("Error occurred:", error)
-  }
-};
-render();
-$('#model-edit').on('change keyup keydown', render);
-$('#template-edit').on('change keyup keydown', render);
-
-//interactive inputs example
-// Load all the templates on the page.
-
-var you = new Backbone.Model({
-  name: 'YourName',
-});
-
-var extremeCharacter = new Backbone.Model({
-  name: 'Tony'
-});
-
-var template = EndDash.getTemplate('inputName', you),
-    template2 = EndDash.getTemplate('whichCharacter', extremeCharacter);
-
-$('#inputs').html(template.el);
-$('#inputs2').html(template2.el);
