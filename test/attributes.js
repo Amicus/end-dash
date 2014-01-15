c58efbb21b977fc55cbd430ddc27f647a2f0795b
Devon Hinton <dhinton@stanford.edu>
2014-01-15 10:18:42 -0800

    EndDash users still would be weirded out that a change event would now not update the DOM, add another failing spec which shows that change:attribute updates the dom, but not change

--- test/attributes.js
+++ test/attributes.js
@@ -19,13 +19,55 @@ describe("Updating the DOM on model changes", function(){
       it("interpolates the 'name' value", function(){
         expect($('#target').hasClass(this.model.get('name'))).to.be(true);
       });
-      describe("changing the model without events and then triggering an event manually", function(){
+      describe("silently changing the model and manually triggering a change event", function(){
         beforeEach(function(){
           this.model.attributes.name = "Zach";
           this.model.trigger('change:name');
         });
         it("interpolates the new value of the attribute", function(){
           expect($('#target').hasClass(this.model.get('name'))).to.be(true);
+        });
+      });
+    });
+  });
+
+  describe("with a markup with a conditional interpolated attribute", function(){
+    beforeEach(function(){
+      this.markup = '<div><div id="target" class="#{name ? dog : cat}"></div></div>';
+    });
+
+    describe("with a backbone model with a non falsy value is bound to the markup", function(){
+      beforeEach(function(){
+        this.model = new Model({name: "not falsy"});
+        this.template = generateTemplate(this.model, this.markup);
+      });
+
+      it("interpolates the true case value of the attribute only", function(){
+        expect($('#target').hasClass('dog')).to.be(true);
+        expect($('#target').hasClass('cat')).to.be(false);
+      });
+
+      describe("silently changing the attribute to be falsy value and manually triggering an attribute change event", function(){
+        beforeEach(function(){
+          this.model.attributes.name = false;
+          this.model.trigger('change:name');
+        });
+
+        it("interpolates the false case value of the attribute only", function(){
+          expect($('#target').hasClass('dog')).to.be(false);
+          expect($('#target').hasClass('cat')).to.be(true);
+        });
+      });
+
+      describe("silently changing the attribute to be falsy value and manually triggering a change event", function(){
+        beforeEach(function(){
+          this.model.attributes.name = false;
+          this.model.trigger('change');
+        });
+
+        it("interpolates the false case value of the attribute only", function(){
+          expect($('#target').hasClass('dog')).to.be(false);
+          expect($('#target').hasClass('cat')).to.be(true);
         });
       });
     });
